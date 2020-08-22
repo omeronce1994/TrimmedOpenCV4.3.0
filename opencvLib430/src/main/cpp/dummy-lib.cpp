e122f6ab4f64d28cc2620efef891c54fc2a77af8
omercohen <omeronce1994@gmail.com>
2020-08-22 22:50:25 +0300

    initial commit

---
+++ opencvLib430/src/main/cpp/dummy-lib.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Omer Cohen on 22/08/2020.
+//
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+whatever(
+        JNIEnv *env,
+        jobject /* this */){
+    std::string hello = "Hello";
+    return env->NewStringUTF(hello.c_str());
+};
